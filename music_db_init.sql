3946b8c8ab087e9c6a2469a392ae1f9774267f0a
Roabesh <roabesh@gmail.com>
2025-05-15 02:12:38 +0300

    Update music_db_init.sql
    Корректировка на отсутствие пустых ответов

--- music_db_init.sql
+++ music_db_init.sql
@@ -60,3 +60,42 @@ INSERT INTO collection_track (collection_id, track_id) VALUES
 (2, 4), -- Best of Pop - Billie Jean
 (3, 5), -- Jazz Classics - What a Wonderful World
 (4, 6); -- Classical Masterpieces - Symphony No. 40
+
+-- Корректировка на отсутствие пустых ответов
+-- Добавляем новые сборники 2018-2020 годов
+INSERT INTO collection (title, release_year) VALUES
+('Modern Hits', 2018),
+('Best of 2019', 2019),
+('Quarantine Songs', 2020);
+
+-- Добавляем новые альбомы 2019-2020 годов
+INSERT INTO album (title, release_year) VALUES
+('New Album 2019', 2019),
+('Pandemic Songs', 2020);
+
+-- Добавляем новые треки для альбомов 2019-2020
+INSERT INTO track (title, duration, album_id) VALUES
+('New Song 2019', 240, 5), -- для альбома New Album 2019
+('Quarantine Blues', 180, 6), -- для альбома Pandemic Songs
+('My Isolation', 200, 6); -- дополнительный трек с "my" для запроса
+
+-- Добавляем связи исполнителей с новыми альбомами
+INSERT INTO artist_album (artist_id, album_id) VALUES
+(2, 5), -- Michael Jackson - New Album 2019
+(1, 6); -- Queen - Pandemic Songs
+
+-- Добавляем связи новых треков со сборниками
+INSERT INTO collection_track (collection_id, track_id) VALUES
+(5, 8), -- Modern Hits - New Song 2019
+(6, 9), -- Best of 2019 - Quarantine Blues
+(7, 8), -- Quarantine Songs - New Song 2019
+(7, 9); -- Quarantine Songs - Quarantine Blues
+
+-- Добавляем дополнительные жанры для исполнителей (для запроса с несколькими жанрами)
+INSERT INTO artist_genre (artist_id, genre_id) VALUES
+(2, 1), -- Michael Jackson также в жанре Rock
+(1, 2); -- Queen также в жанре Pop
+
+-- Добавляем трек не входящий в сборники (для соответствующего запроса)
+INSERT INTO track (title, duration, album_id) VALUES
+('Lonely Track', 185, 1); -- трек не будет добавлен в collection_track
